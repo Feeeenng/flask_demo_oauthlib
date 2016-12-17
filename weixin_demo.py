e8329fd2a2f1b385dd9ad9cbfc3007ab7e17dad1
332007851 <z332007851@qq.com>
2016-12-17 16:15:16 +0800

    增加微信回调地址的说明

--- weixin_demo.py
+++ weixin_demo.py
@@ -14,7 +14,9 @@ app = Flask(__name__)
 
 APP_ID = ''
 APP_SECRET = ''
-REDIRECT_URI = '' #这里是填写你在微信里的回调地址
+
+#假设我本地的IP为192.168.0.50， 微信开放平台填写192.168.0.50,而这里的回调地址就是http://192.168.0.50/authorization,并且一定要以http:// or https://打头
+REDIRECT_URI = 'http://192.168.0.50/authorization'
 
 @app.route("/authorization")
 def authorization():
